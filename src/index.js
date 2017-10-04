205fd48882523b661edf37b25201905ab2859386
Shravan Shetty <shravanshetty.ec@gmail.com>
2017-10-03 23:42:01 -0500

    Added reducers for all state variables.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import C from './constants';
-import { allSkiDays, goal } from './initialState.json';
+import { allSkiDays } from './store/reducers';
+
+const state = [{
+    "resort": "Kirkwood",
+    "date": "2016-12-7",
+    "powder": true,
+    "backcountry": false
+}, {
+    "resort": "Squaw Valley",
+    "date": "2016-12-8",
+    "powder": false,
+    "backcountry": false
+}];
+
+const action = {
+    type: C.REMOVE_DAY,
+    payload: "2016-12-7"
+}
+
+const nextState = allSkiDays(state, action);
 
 console.log(`
-    Ski Day Counter
-    ===============
-    The goal is ${goal} days
-    Initially there are ${allSkiDays.length} ski days in state
-
-    Constatnts (actions)
-    --------------------
-    ${Object.keys(C).join('\n      ')}
+    initial goal: ${JSON.stringify(state)}
+    action: ${JSON.stringify(action)}
+    new goal: ${JSON.stringify(nextState)}
 `)
