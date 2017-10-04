5f77a2645c2fd30dab6cb7b969b4c769c66286f9
Shravan Shetty <shravanshetty.ec@gmail.com>
2017-10-03 22:13:30 -0500

    Setup basic structure and added dependencies.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import C from './constants';
+import { allSkiDays, goal } from './initialState.json';
+
+console.log(`
+    Ski Day Counter
+    ===============
+    The goal is ${goal} days
+    Initially there are ${allSkiDays.length} ski days in state
+
+    Constatnts (actions)
+    --------------------
+    ${Object.keys(C).join('\n      ')}
+`)
